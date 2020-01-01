fc6139a0e604ff64ad07eb609770a240663960d9
Spyynox <maxime.mbiya-kiaku@supinternet.fr>
2020-01-01 13:02:12 +0100

    add role access and Student Controller

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -5,15 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class StudentController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/etudiant", name="student")
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('student/index.html.twig', [
+            'controller_name' => 'StudentController',
         ]);
     }
 }
