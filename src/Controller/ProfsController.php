f1feb3b2534252e3699213d14ea26d7d7fe49b61
Spyynox <maxime.mbiya-kiaku@supinternet.fr>
2020-01-07 21:47:43 +0100

    finish css prof

--- src/Controller/ProfsController.php
+++ src/Controller/ProfsController.php
@@ -24,79 +24,13 @@ class ProfsController extends AbstractController
     /**
      * @Route("/", name="app_profs_index", methods={"GET"})
      */
-    public function index(MatterRepository $matterRepository, StudentUserRepository $studentUserRepository): Response
+    public function index(MatterRepository $matterRepository, StudentUserRepository $studentUserRepository, UserRepository $userRepository): Response
     {
         return $this->render('profMatter/index.html.twig', [
             'matters' => $matterRepository->findAll(),
             'studentUsers' => $studentUserRepository->findAll(),
+            'users' => $userRepository->findAll(),
         ]);
-    }
-
-    /**
-     * @Route("/new", name="matter_new", methods={"GET","POST"})
-     */
-    public function new(Request $request): Response
-    {
-        $matter = new Matter();
-        $form = $this->createForm(MatterType::class, $matter);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($matter);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_profs_index');
-        }
-
-        return $this->render('profMatter/new.html.twig', [
-            'matter' => $matter,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="matter_show", methods={"GET"}, requirements={"id":"\d+"})
-     */
-    public function show(Matter $matter): Response
-    {
-        return $this->render('profMatter/show.html.twig', [
-            'matter' => $matter,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="matter_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Matter $matter): Response
-    {
-        $form = $this->createForm(MatterType::class, $matter);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('matter_index');
-        }
-
-        return $this->render('profMatter/edit.html.twig', [
-            'matter' => $matter,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="matter_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, Matter $matter): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$matter->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($matter);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('matter_index');
     }
 
     /**
